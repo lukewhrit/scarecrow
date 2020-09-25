d82091d172b19f251fd7bac06b6f47b4bcbc063c
Luke Whrit <lukewhrit@gmail.com>
2020-09-25 18:37:33 -0400

    fix(lib/compile): use proper goldmark output

--- lib/compile.go
+++ lib/compile.go
@@ -37,7 +37,10 @@ type Document struct {
 // Compile turns a markdown document into a fully-formed HTML file using a layout
 func (d *Document) Compile(dir string) (err error) {
 	d.Content, err = ioutil.ReadFile(d.FullPath)
-	err = goldmark.Convert(d.Content, bytes.NewBuffer(d.Content))
+
+	var buf *bytes.Buffer
+	err = goldmark.Convert(d.Content, buf)
+	d.Content = buf.Bytes()
 
 	path, err := filepath.Rel(dir, d.FullPath)
 	folder := strings.Split(path, string(filepath.Separator))[0]
